d2bbbef5f497e25d3c91b7855e755971703c0139
Shivakant <kants6397@gmail.com>
2025-03-22 16:51:18 +0530

    code readability

--- services/deadline-detector/server.js
+++ services/deadline-detector/server.js
@@ -12,15 +12,6 @@ app.get("/", (req, res) => {
   res.send("Deadline Detector Service");
 });
 
-// await sendDeadlineEmail({
-//   to: "kants6397@gmail.com",
-//   from: "shivakant52@nitmanipur.ac.in",
-//   taskName: "Prepare Quarterly Report",
-//   deadline: "2025-03-20",
-//   taskId: "TASK-101",
-//   cc: ["emimicshiva11@gmail.com", "manager@example.com"],
-// });
-
 app.use("/api/v1", emailRoute);
 
 app.use("/api/v1", detectRoute);
